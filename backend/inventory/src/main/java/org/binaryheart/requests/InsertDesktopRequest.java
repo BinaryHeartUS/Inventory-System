d1b272d9e075ba7d494fd4d7871072a3c5703ccb
rhit-lucasja <lucasja@rose-hulman.edu>
2026-05-01 01:46:32 +0000

    made it so swagger endpoints use pretty print rather than database enum value

--- backend/inventory/src/main/java/org/binaryheart/requests/InsertDesktopRequest.java
+++ backend/inventory/src/main/java/org/binaryheart/requests/InsertDesktopRequest.java
@@ -1,10 +1,9 @@
 package org.binaryheart.requests;
 
-import org.binaryheart.enums.Status;
-
 import java.time.LocalDate;
 
-public record InsertDesktopRequest(int chapterId, String manufacturer, String model, int year, Status status,
-        Integer assetId, String cpu, Integer ram, String ramGeneration, Integer storageAmount, String storageType,
-        Double value, LocalDate acquisitionDate, Integer recipientId, Integer donorId, Boolean hasWifi) {
+public record InsertDesktopRequest(int chapterId, String manufacturer, String model, int year, String status,
+                Integer assetId, String cpu, Integer ram, String ramGeneration, Integer storageAmount,
+                String storageType, Double value, LocalDate acquisitionDate, Integer recipientId, Integer donorId,
+                Boolean hasWifi) {
 }
