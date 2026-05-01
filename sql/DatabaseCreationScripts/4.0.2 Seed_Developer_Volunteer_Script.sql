7c408d5a963b2db7d45bc31ca893ac0925206f28
2tmb2 <tal@belkind.net>
2026-05-01 15:29:20 +0000

    Add viewer role to user seed

--- sql/DatabaseCreationScripts/4.0.2 Seed_Developer_Volunteer_Script.sql
+++ sql/DatabaseCreationScripts/4.0.2 Seed_Developer_Volunteer_Script.sql
@@ -2,6 +2,7 @@ DO $$
 DECLARE
     v_volunteer_id INTEGER;
     v_role_id INTEGER;
+    v_viewer_id INTEGER;
     v_national_id INTEGER;
 BEGIN
     SELECT ID INTO v_role_id FROM Role WHERE Name = 'Admin';
@@ -20,6 +21,22 @@ BEGIN
 
     INSERT INTO Affiliated_With (Volunteer_ID, Chapter_ID, Role_ID)
     VALUES (v_volunteer_id, v_national_id, v_role_id)
+    ON CONFLICT (Volunteer_ID, Chapter_ID) DO NOTHING;
+
+    SELECT ID INTO v_viewer_id FROM Role WHERE Name = 'Viewer';
+
+    INSERT INTO Volunteer (Name, Username, Password_Hash)
+    VALUES (
+        'viewer',
+        'viewer',
+        '$2a$12$VNqyG0.YL3/RFOC8/PAyrOLrox7A/EWi0KgrK6oUg4Cq8slFr0yHm'
+    )
+    ON CONFLICT (Username) DO NOTHING;
+
+    SELECT ID INTO v_volunteer_id FROM Volunteer WHERE Username = 'viewer';
+
+    INSERT INTO Affiliated_With (Volunteer_ID, Chapter_ID, Role_ID)
+    VALUES (v_volunteer_id, v_national_id, v_viewer_id)
     ON CONFLICT (Volunteer_ID, Chapter_ID) DO NOTHING;
 END;
 $$;
