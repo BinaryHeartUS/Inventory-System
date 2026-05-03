fa47b2cf257c35c3c521d756222fdd29cff8ce9f
2tmb2 <tal@belkind.net>
2026-05-03 01:22:45 +0000

    Fix null handling for containedIn field in getAllParts method

--- backend/inventory/src/main/java/org/binaryheart/repositories/PartRepository.java
+++ backend/inventory/src/main/java/org/binaryheart/repositories/PartRepository.java
@@ -28,6 +28,7 @@ public class PartRepository {
             String desc = res.getString("description");
             boolean wasPurchased = res.getBoolean("wasPurchased");
             Integer containedIn = res.getInt("containedIn");
+            if (res.wasNull()) containedIn = null;
             int chapterId = res.getInt("chapterID");
             Date acquisitionDate = res.getDate("acquisitionDate");
             Double value = res.getDouble("value");
