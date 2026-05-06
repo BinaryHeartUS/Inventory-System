7d59d7eec8e0e2a4723e61f317f4629dbe473bb5
Jack Lucas <jackallen2024@gmail.com>
2026-05-06 17:42:46 +0000

    added PartNotFoundException to fix import resolution

---
+++ backend/inventory/src/main/java/org/binaryheart/exceptions/PartNotFoundException.java
@@ -0,0 +1,7 @@
+package org.binaryheart.exceptions;
+
+public class PartNotFoundException extends RuntimeException {
+    public PartNotFoundException(String message) {
+        super(message);
+    }
+}
