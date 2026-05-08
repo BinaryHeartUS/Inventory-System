235ce40cbb69a3bb95256bdf2b7a6383b0702d49
Jack Lucas <jackallen2024@gmail.com>
2026-05-08 20:12:24 +0000

    i lied here's real implementation

--- backend/inventory/src/main/java/org/binaryheart/services/PartyService.java
+++ backend/inventory/src/main/java/org/binaryheart/services/PartyService.java
@@ -1,5 +1,26 @@
 package org.binaryheart.services;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.binaryheart.exceptions.BadArgumentException;
+import org.binaryheart.exceptions.PartyNotFoundException;
+import org.binaryheart.repositories.PartyRepository;
+import org.binaryheart.responses.GetPartyResponse;
+
 public class PartyService {
 
+    private final PartyRepository repository = new PartyRepository();
+
+    public List<GetPartyResponse> getAllParties() throws SQLException {
+        return repository.getAllParties().collect(Collectors.toList());
+    }
+
+    public GetPartyResponse getParty(int id) throws BadArgumentException, PartyNotFoundException, SQLException {
+        if (id <= 0) {
+            throw new BadArgumentException("Party ID must be positive");
+        }
+        return repository.getParty(id);
+    }
 }
