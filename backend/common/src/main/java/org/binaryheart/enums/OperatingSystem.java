8931dbb63a720fc91893251d038b5ff4b45f9ddf
2tmb2 <tal@belkind.net>
2026-05-08 19:40:52 +0000

    Fixed importer

--- backend/common/src/main/java/org/binaryheart/enums/OperatingSystem.java
+++ backend/common/src/main/java/org/binaryheart/enums/OperatingSystem.java
@@ -2,7 +2,7 @@ package org.binaryheart.enums;
 
 public enum OperatingSystem {
     WINDOWS_11("Windows 11"), VANILLA_OS("Vanilla OS"), MACOS("macOS"), IPAD_OS("iPadOS"), IOS("iOS"),
-    UNKNOWN("Unknown");
+    UNKNOWN("Unknown"), ARCHLINUX("Arch Linux");
 
     private final String databaseValue;
 
