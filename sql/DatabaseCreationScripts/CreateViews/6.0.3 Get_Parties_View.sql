7a801296bb41b872ad3815def2f6c273ed01bb31
Jack Lucas <jackallen2024@gmail.com>
2026-05-08 20:43:12 +0000

    get all parties view done

---
+++ sql/DatabaseCreationScripts/CreateViews/6.0.3 Get_Parties_View.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE VIEW Get_Parties
+AS
+SELECT p.ID, p.Name, p.Location, per.Email AS IndividualEmail, o.ContactName, o.ContactEmail
+FROM Party p
+LEFT JOIN Person per ON p.ID = per.ID
+LEFT JOIN Organization o ON p.ID = o.ID
