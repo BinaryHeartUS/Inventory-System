45e2bad8279b2a096272e6fac35888ebb102e0c7
Hunter Snyder <snyderh1@rose-hulman.edu>
2026-05-11 02:54:23 +0000

    updated insert person sproc to use inout id

--- sql/DatabaseCreationScripts/CreateStoredProcedures/3.1.5 Insert_Person_Procedure.sql
+++ sql/DatabaseCreationScripts/CreateStoredProcedures/3.1.5 Insert_Person_Procedure.sql
@@ -1,7 +1,7 @@
 DROP PROCEDURE IF EXISTS Insert_Person;
 
 CREATE OR REPLACE PROCEDURE Insert_Person(
-    OUT p_ID INTEGER,
+    INOUT p_ID INTEGER,
     IN p_Name Name_Type,
     IN p_Location Address,
     IN p_Email Email_Type
